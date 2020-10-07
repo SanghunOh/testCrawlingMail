0467da633510d29f077a58cb111632f5bc05aa78
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-10-07 23:18:23 +0900

    change some files

--- bs4/inserfromhtmltomongoDB.py
+++ bs4/inserfromhtmltomongoDB.py
@@ -6,22 +6,23 @@ from pymongo import MongoClient
 path = 'datas/sample03.html'
 db_url = 'mongodb://192.168.0.6:27017/'
 
-with open(path) as fp:						        # shared github
-    soup = BeautifulSoup(fp, features='lxml')
-    links = soup.select('p[id]')					# 속성 존재 여부 검색
+def scheduler_sample():
+    with open(path) as fp:						        # shared github
+        soup = BeautifulSoup(fp, features='lxml')
+        links = soup.select('p[id]')					# 속성 존재 여부 검색
 
-    # connect MongoDB
-    with MongoClient(db_url) as client:
-        sampledb = client['sample03db']						# get Database
-        # check collections
-        if ("sampleCollection" not in sampledb.list_collection_names()):
-            sampledb.create_collection('sampleCollection')
+        # connect MongoDB
+        with MongoClient(db_url) as client:
+            sampledb = client['sample03db']						# get Database
+            # check collections
+            if ("sampleCollection" not in sampledb.list_collection_names()):
+                sampledb.create_collection('sampleCollection')
 
-        title = ''
-        link = ''
-        for link in links:
-            title = str.strip(link.get_text())
-            link = link['id']
-            data = {'title': title, 'id': link}
-            infor = sampledb.sampleCollection.insert_one(data)
-
+            title = ''
+            link = ''
+            for link in links:
+                title = str.strip(link.get_text())
+                link = link['id']
+                data = {'title': title, 'id': link}
+                infor = sampledb.sampleCollection.insert_one(data)
+                print(infor.inserted_id)
